8e965b9a96d36922912083841e1f11a17b316eda
farizdotid <contact.farizramadhan@gmail.com>
2023-12-25 19:41:45 +0700

    first commit :tada:

---
+++ service-worker.js
@@ -0,0 +1,65 @@
+const CACHE_NAME = 'direct-whatsapp-chat-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html', // Add other HTML files if needed
+  '/manifest.json',
+  '/src/img/icon_direct_chat_wa_192.png', // Adjust the path to your app icon,
+  '/styles.css',
+  // Add other assets like stylesheets, scripts, and images
+];
+
+// Install the service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate the service worker and clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch assets from cache or network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return the response from the cache
+        if (response) {
+          return response;
+        }
+
+        // Clone the request to make a network request
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then((response) => {
+          // Check if we received a valid response
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          // Clone the response to cache it
+          const responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then((cache) => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
+      })
+  );
+};
